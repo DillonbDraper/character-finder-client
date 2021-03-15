65f65d2afab271a63a83a812c5db75c6706e34d8
Dillon Draper <dillonbdraper@gmail.com>
2021-03-15 11:56:33 -0500

    Fleshed out project file structure further and added more autocompletes for other search categoreis, but now getting likely async issue with other autocompletes not having data on time.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,7 +1,20 @@
 import React, { useContext, useEffect } from "react"
 import { Route } from "react-router-dom"
 import { CharacterProvider } from "./characters/CharacterProvider"
+import { FictionProvider } from "./fictions/FictionProvider"
+import { SeriesProvider } from "./series/SeriesProvider"
+import { AuthorProvider } from "./authors/AuthorProvider"
 import { CharacterList } from "./characters/CharacterList"
+import { CharacterDetail } from "./characters/CharacterDetail"
+import { AuthorDetail } from "./authors/AuthorDetail"
+import { FictionDetail } from "./fictions/FictionDetail"
+import { SeriesDetail } from "./series/SeriesDetail"
+import { SeriesForm } from "./series/SeriesForm"
+import { FictionForm } from "./fictions/FictionForm"
+import { AuthorForm } from "./authors/AuthorForm"
+import { CharacterForm } from "./characters/CharacterForm"
+import { CreationHub } from "./CreationHub"
+
 
 export const ApplicationViews = (props) => {
     return (
@@ -9,10 +22,39 @@ export const ApplicationViews = (props) => {
         <>
 
             <CharacterProvider>
-                <Route exact path="/" render={
-                    () =>
-                        <CharacterList />
-                } />
+                <AuthorProvider>
+                    <FictionProvider>
+                        <SeriesProvider>
+                            <Route exact path="/" render={
+                                () =>
+                                    <CharacterList />
+                            } />
+
+                            <Route exact path="/characters:character_id(\d+)/" render={
+                                () =>
+                                    <CharacterDetail />
+                            } />
+
+
+                            <Route exact path="/authors:author_id(\d+)/" render={
+                                () =>
+                                    <AuthorDetail />
+                            } />
+
+
+                            <Route exact path="/fictions:fiction_id(\d+)/" render={
+                                () =>
+                                    <FictionDetail />
+                            } />
+
+
+                            <Route exact path="/series/:series_id(\d+)/" render={
+                                () =>
+                                    <SeriesDetail />
+                            } />
+                        </SeriesProvider>
+                    </FictionProvider>
+                </AuthorProvider>
             </CharacterProvider>
 
         </>
--- src/components/characters/CharacterList.js
+++ src/components/characters/CharacterList.js
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from "react"
 import { CharacterContext } from "./CharacterProvider.js"
-import { useParams, useHistory } from "react-router-dom"
+import { FictionContext } from "../fictions/FictionProvider.js"
+import { AuthorContext } from "../authors/AuthorProvider.js"
+import { SeriesContext } from "../series/SeriesProvider.js"
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -18,14 +20,27 @@ import Button from '@material-ui/core/Button';
 
 export const CharacterList = () => {
   const { characters, filteredCharacters, getCharacters, getCharactersWithParams } = useContext(CharacterContext)
-  const [characterValue, setCharacterValue] = useState(null)
-  const [inputValue, setInputValue] = React.useState('')
+  const { getFictions, fictions } = useContext(FictionContext)
+  const { getSeries, series } = useContext(SeriesContext)
+  const { getAuthors, authors } = useContext(AuthorContext)
 
-  const params = useParams()
-  const history = useHistory()
+  const [characterValue, setCharacterValue] = useState(null)
+  const [characterInputValue, setCharacterInputValue] = useState('')
+
+  const [fictionValue, setFictionValue] = useState(null)
+  const [fictionInputValue, setFictionInputValue] = useState('')
+
+  const [authorValue, setAuthorValue] = useState(null)
+  const [authorInputValue, setAuthorInputValue] = useState('')
+
+  const [seriesValue, setSeriesValue] = useState(null)
+  const [seriesInputValue, setSeriesInputValue] = useState('')
+
 
   useEffect(() => {
-    getCharacters()
+    getCharacters().then(getFictions).then(getSeries).then(getAuthors).then(() => {
+      console.log(characters)
+    })
   }, [])
 
   useEffect(() => {
@@ -65,9 +80,9 @@ export const CharacterList = () => {
 
   return (
     <>
-      <Container maxWidth="xl" style={{ backgroundColor: '#cfe8fc', height: '10vh' }}>
+      <Container maxWidth="xl" style={{ backgroundColor: '#cfe8fc', height: '10vh', display: 'flex' }}>
         <Autocomplete
-          id="positions"
+          id="characters"
           options={characters}
           getOptionLabel={(char) => {
             if (!char.name || char === {}) {
@@ -86,9 +101,90 @@ export const CharacterList = () => {
             setCharacterValue(newValue);
           }}
 
-          inputValue={inputValue}
+          inputValue={characterInputValue}
           onInputChange={(event, newInputValue) => {
-            setInputValue(newInputValue);
+            setCharacterInputValue(newInputValue);
+          }}
+          renderInput={(params) => <TextField {...params} label="Position" variant="outlined" />}
+        />
+
+        <Autocomplete
+          id="fictions"
+          options={fictions}
+          getOptionLabel={(fict) => {
+            if (!fict.title || fict === {}) {
+              return ""
+            } else {
+              return fict.title
+            }
+          }
+          }
+          getOptionSelected={(fiction, fictionValue) => fiction === fictionValue}
+          style={{
+            width: 300,
+          }}
+          value={fictionValue}
+          onChange={(event, newValue) => {
+            setFictionValue(newValue);
+          }}
+
+          inputValue={fictionInputValue}
+          onInputChange={(event, newInputValue) => {
+            setFictionInputValue(newInputValue);
+          }}
+          renderInput={(params) => <TextField {...params} label="Position" variant="outlined" />}
+        />
+
+        <Autocomplete
+          id="authors"
+          options={authors}
+          getOptionLabel={(auth) => {
+            if (!auth.name || auth === {}) {
+              return ""
+            } else {
+              return auth.name
+            }
+          }
+          }
+          getOptionSelected={(author, authorValue) => author === authorValue}
+          style={{
+            width: 300,
+          }}
+          value={authorValue}
+          onChange={(event, newValue) => {
+            setAuthorValue(newValue);
+          }}
+
+          inputValue={authorInputValue}
+          onInputChange={(event, newInputValue) => {
+            setAuthorInputValue(newInputValue);
+          }}
+          renderInput={(params) => <TextField {...params} label="Position" variant="outlined" />}
+        />
+
+        <Autocomplete
+          id="series"
+          options={series}
+          getOptionLabel={(ser) => {
+            if (!ser.title || ser === {}) {
+              return ""
+            } else {
+              return ser.title
+            }
+          }
+          }
+          getOptionSelected={(series, seriesValue) => series === seriesValue}
+          style={{
+            width: 300,
+          }}
+          value={seriesValue}
+          onChange={(event, newValue) => {
+            setSeriesValue(newValue);
+          }}
+
+          inputValue={seriesInputValue}
+          onInputChange={(event, newInputValue) => {
+            setSeriesInputValue(newInputValue);
           }}
           renderInput={(params) => <TextField {...params} label="Position" variant="outlined" />}
         />
