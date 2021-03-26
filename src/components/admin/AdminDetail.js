35aa579bfe727550aa4b70c4a242c8c74ed0d29c
Dillon Draper <dillonbdraper@gmail.com>
2021-03-25 23:30:36 -0500

    Introduced code to admin-detail that allows for associations to be updated as well as character data. Not as DRY as I would like, but it works.

--- src/components/admin/AdminDetail.js
+++ src/components/admin/AdminDetail.js
@@ -10,7 +10,7 @@ export const AdminDetail = () => {
     const params = useParams()
     const history = useHistory()
 
-    const { character, getCharacterById, checkForMatch, checkForMatchOriginal, secondCharacter, approveCharacterEdit, rejectCharacterEdit } = useContext(CharacterContext)
+    const { character, getCharacterById, checkForMatch, checkForMatchOriginal, secondCharacter, approveCharacterEdit, addCharacterAssociations, rejectCharacterEdit } = useContext(CharacterContext)
 
 
     useEffect(() => {
@@ -18,6 +18,9 @@ export const AdminDetail = () => {
             checkForMatch(params.characterId)
         })
     }, [])
+
+    let relationshipObject = {}
+
 
     useEffect(() => { }, [secondCharacter])
 
@@ -27,7 +30,7 @@ export const AdminDetail = () => {
                 <div>
                     <h2>Old Version</h2>
                     <h4>{secondCharacter.name}</h4>
-                    {secondCharacter.image ? <img style={{ maxWidth: '100%' }} src={secondCharacter.image} /> : <img style={{ maxWidth: '40%', height: '10%' }} src={`http://localhost:8000/media/blank-profile-picture-973460_1280.png`} />}
+                    {secondCharacter.image ? <img style={{ maxWidth: '40%', height: '10%' }} src={secondCharacter.image} /> : <img style={{ maxWidth: '40%', height: '10%' }} src={`http://localhost:8000/media/blank-profile-picture-973460_1280.png`} />}
                     <p>AKA: {secondCharacter.alias} </p>
                     <p>Age: {secondCharacter.age}</p>
                     <p>Born on: {secondCharacter.born_on}</p>
@@ -40,20 +43,20 @@ export const AdminDetail = () => {
                     <p>Created by:  {secondCharacter.creators[0] ? secondCharacter.creators[0].name : "NA"}</p>
                     <p>Appears in the series:  {secondCharacter.series[0] ? secondCharacter.series[0].title : "NA"}</p>
                     <p>Has Character relationship:  {secondCharacter.associations[0] ? secondCharacter.associations[0].char_one ? secondCharacter.associations[0].char_one.name :
-                    secondCharacter.associations[0].char_two.name : "NA"}</p>
-                    <p style={{maxWidth: '60%'}}>Relationship description: {secondCharacter.associations[0] ? secondCharacter.associations[0].description : "NA"}</p>
+                        secondCharacter.associations[0].char_two.name : "NA"}</p>
+                    <p style={{ maxWidth: '60%' }}>Relationship description: {secondCharacter.associations[0] ? secondCharacter.associations[0].description : "NA"}</p>
                     <Button color="primary" variant="contained"
-                    onClick={() => {
-                        rejectCharacterEdit(secondCharacter.id, character)
-                        history.push('/')
-                    }}
-                >Reject Edit</Button>
+                        onClick={() => {
+                            rejectCharacterEdit(secondCharacter.id, character)
+                            history.push('/')
+                        }}
+                    >Reject Edit</Button>
                 </div>
 
                 <div>
                     <h2>New Version</h2>
                     <h4>{character.name}</h4>
-                    {character.image ? <img style={{ maxWidth: '40%', height: '10%'  }} src={character.image} /> : <img style={{ maxWidth: '40%', height: '10%' }} src={`http://localhost:8000/media/blank-profile-picture-973460_1280.png`} />}
+                    {character.image ? <img style={{ maxWidth: '40%', height: '10%' }} src={character.image} /> : <img style={{ maxWidth: '40%', height: '10%' }} src={`http://localhost:8000/media/blank-profile-picture-973460_1280.png`} />}
                     <p>AKA: {character.alias} </p>
                     <p>Age: {character.age}</p>
                     <p>Born on: {character.born_on}</p>
@@ -66,15 +69,38 @@ export const AdminDetail = () => {
                     <p>Created by:  {character.creators[0] ? character.creators[0].name : "NA"}</p>
                     <p>Appears in the series:  {character.series[0] ? character.series[0].title : "NA"}</p>
                     <p>Has Character relationship:  {character.associations[0] ? character.associations[0].char_one ? character.associations[0].char_one.name :
-                    character.associations[0].char_two.name : "NA"}</p>
+                        character.associations[0].char_two.name : "NA"}</p>
                     <p>Relationship description: {character.associations[0] ? character.associations[0].description : "NA"}</p>
                     <Button color="primary" variant="contained"
-                    onClick={() => {
-                        approveCharacterEdit(secondCharacter.id, character).then(() => checkForMatchOriginal(secondCharacter.id))
-                        history.push('/')
-                    }}
-                >
-                    Approve Edit</Button>
+                        onClick={() => {
+                            approveCharacterEdit(secondCharacter.id, character).then(() => {
+                                if (character.works[0] || character.series[0] || character.associations[0]) {
+                                    if (character.works[0]) {
+                                        relationshipObject.fictions = character.works
+                                    }
+                                    if (character.series[0]) {
+                                        relationshipObject.series = character.series[0]
+                                    }
+                                    if (character.associations[0]) {
+                                        if (character.associations[0].char_one) {
+                                            relationshipObject.characters = character.associations[0].char_one
+                                            relationshipObject.description = character.associations[0].description
+                                        }
+                                        else if (character.associations[0].char_two) {
+                                            relationshipObject.characters = character.associations[0].char_two
+                                            relationshipObject.description = character.associations[0].description
+
+                                        }
+                                    }
+
+                                }
+                                addCharacterAssociations(secondCharacter.id, relationshipObject)
+                            }).then(() =>
+                                checkForMatchOriginal(secondCharacter.id))
+                            history.push('/')
+                        }}
+                    >
+                        Approve Edit</Button>
                 </div>
 
 
@@ -82,8 +108,8 @@ export const AdminDetail = () => {
             </Container>
             <Container style={{ display: 'flex', justifyContent: 'space-between' }}>
 
-                
-              
+
+
             </Container>
 
         </Container>
